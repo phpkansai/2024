c978035d6518b29c2cc69e34c0aec1c9766eb9ab
Y-Kanoh <headgcf.es@gmail.com>
2023-11-03 00:03:42 +0900

    YesNo診断
    パス変更

--- yes-no-questions/quasar.config.js
+++ yes-no-questions/quasar.config.js
@@ -67,7 +67,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/yes-no-test',
       // analyze: true,
       // env: {},
       // rawDefine: {}
