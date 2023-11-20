9cf9bbef89313739b7a5715e3fdff28e73cd0f8e
Y-Kanoh <headgcf.es@gmail.com>
2023-11-20 23:35:54 +0900

    YesNo診断
    説明文と注意書きを追加

--- yes-no-questions/src/pages/IndexPage.vue
+++ yes-no-questions/src/pages/IndexPage.vue
@@ -1,7 +1,7 @@
 <template>
-  <q-page class="fullscreen">
+  <q-page class="">
     <div class="row justify-center">
-      <div class="col-xs-11 absolute-center text-center q-my-md">
+      <div class="col-xs-11 text-center q-my-md q-mt-lg-md">
 
         <div class="row justify-center">
           <div class="col-xs-11 col-md-5 text-center q-my-md">
@@ -14,12 +14,20 @@
 
           <div class="col-xs-11 col-md-5 ">
             <div class="row justify-center">
-              <div class="col-xs-11 col-md-11 text-center q-my-md q-mx-lg">
-                説明が入るところ説明が入るところ説明が入るところ説明が入るところ説明が入るところ説明が入るところ説明が入るところ説明が入るところ説明が入るところ説明が入るところ説明が入るところ説明が入るところ説明が入るところ
+              <div class="col-xs-11 col-md-11 text-center q-mx-lg">
+                <p class="text-h6">あなたにぴったりのトークは？</p>
+                <p>
+                  2/11（日）にどのトークを聞けばいいか迷っていませんか？
+                  簡単な質問に答えるだけで、あなたにぴったりのトークを診断します。
+                  聞くべきトークを見つけて、PHPカンファレンス関西2024を楽しみましょう！
+                </p>
+                <p class="text-caption text-grey-7">
+                  本診断はPHPカンファレンス関西2024実行委員会が作成したものです。実際の発表内容と異なる場合があります。
+                </p>
               </div>
             </div>
             <div class="row justify-center">
-              <div class="col-11 q-my-md q-mx-md">
+              <div class="col-11 q-mx-md">
                 <q-btn
                   :to="{name: 'QuestionsPage'}"
                   push
