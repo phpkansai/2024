e4c2f8ee4ac5cdda1329dde2fef16b5717c75b22
Y-Kanoh <headgcf.es@gmail.com>
2023-12-08 23:54:30 +0900

    プロポーザルのSNS共有用静的ページ作成

--- yes-no-questions/createCardPagesByImageFiles.sh
+++ yes-no-questions/createCardPagesByImageFiles.sh
@@ -9,8 +9,11 @@ fileList=`echo $lsResult | tr " " "\n"`
 # fileListのループ
 for file in $fileList
 do
+  # なぜかGoogleドライブからpngファイルをまとめでダウンロードするとファイル拡張子前に"_"がついたので削除
+  fileName=`echo $file | sed -e "s/_//g"`
+  mv ./public/results/$file ./public/results/$fileName
   # ファイル名から拡張子を除いた名前を取得
-  uuid=`echo $file | sed -e "s/.png//g"`
+  uuid=`echo $fileName | sed -e "s/.png//g"`
   cat ./data/snsCardPageTemplate.html | sed -e "s/__UUID__/$uuid/g" > ./public/results/$uuid.html
 done
 
