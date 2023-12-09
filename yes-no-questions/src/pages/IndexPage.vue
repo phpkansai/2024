f1299a9e6b475833a9aa25a272326415fd8e0659
Y-Kanoh <headgcf.es@gmail.com>
2023-12-09 18:02:18 +0900

    ちょうぜつエンジニアを追加
    画像の読み込みが遅い問題に抵抗。

--- yes-no-questions/src/pages/IndexPage.vue
+++ yes-no-questions/src/pages/IndexPage.vue
@@ -9,57 +9,62 @@
 
         <div class="row justify-center">
           <div class="col-xs-11 col-md-5 text-center q-my-md">
-            <img
-              alt="おすすめトーク診断"
-              src="~assets/logo.png"
-              @load="hideLoad"
-              style="width:100%; max-width: 300px; border-radius: 15px;"
-            >
+            <q-intersection transition="scale">
+              <img
+                v-show="!loading"
+                alt="おすすめトーク診断"
+                src="~assets/logo.png"
+                @load="hideLoad"
+                style="width:100%; max-width: 300px; border-radius: 15px;"
+              >
+            </q-intersection>
           </div>
 
-          <div class="col-xs-11 col-md-5 memory-chan-stage">
-            <div class="row justify-center">
-              <div class="col-xs-11 col-md-11 text-center q-mx-lg">
-                <img
-                    src="~/assets/memory-chan/memory-chan01.png"
-                    class="memory-chan01"
-                    alt="ちょうぜつエンジニア"
-                />
-                <img
-                    src="~/assets/memory-chan/memory-chan02.png"
-                    class="memory-chan02"
-                    alt="ちょうぜつエンジニア"
-                />
-                <p class="text-h6">あなたにぴったりのトークは？</p>
-                <p>
-                  2/11（日）にどのトークを聞けばいいか迷っていませんか？
-                  簡単な質問に答えるだけで、あなたにぴったりのトークを診断します。
-                  聞くべきトークを見つけて、PHPカンファレンス関西2024を楽しみましょう！
-                </p>
-                <p class="text-caption text-grey-7">
-                  本診断はPHPカンファレンス関西2024実行委員会が作成したものです。実際の発表内容と異なる場合があります。
-                </p>
+          <q-intersection transition="fade">
+            <div class="col-xs-11 col-md-5 memory-chan-stage">
+              <div class="row justify-center">
+                <div class="col-xs-11 col-md-11 text-center q-mx-lg">
+                  <img
+                      src="~/assets/memory-chan/memory-chan01.png"
+                      class="memory-chan01"
+                      alt="ちょうぜつエンジニア"
+                  />
+                  <img
+                      src="~/assets/memory-chan/memory-chan02.png"
+                      class="memory-chan02"
+                      alt="ちょうぜつエンジニア"
+                  />
+                  <p class="text-h6">あなたにぴったりのトークは？</p>
+                  <p>
+                    2/11（日）にどのトークを聞けばいいか迷っていませんか？
+                    簡単な質問に答えるだけで、あなたにぴったりのトークを診断します。
+                    聞くべきトークを見つけて、PHPカンファレンス関西2024を楽しみましょう！
+                  </p>
+                  <p class="text-caption text-grey-7">
+                    本診断はPHPカンファレンス関西2024実行委員会が作成したものです。実際の発表内容と異なる場合があります。
+                  </p>
+                </div>
+              </div>
+              <div class="row justify-center">
+                <div class="col-11 q-mx-md">
+                  <q-btn
+                    :to="{name: 'QuestionsPage'}"
+                    push
+                    class="full-width"
+                    color="white"
+                    size="22px"
+                    text-color="primary"
+                    label="診断開始"
+                  />
+                </div>
+              </div>
+              <div class="row justify-center">
+                <div class="col-11 q-mt-lg q-mx-md">
+                  <back-web-site-button />
+                </div>
               </div>
             </div>
-            <div class="row justify-center">
-              <div class="col-11 q-mx-md">
-                <q-btn
-                  :to="{name: 'QuestionsPage'}"
-                  push
-                  class="full-width"
-                  color="white"
-                  size="22px"
-                  text-color="primary"
-                  label="診断開始"
-                />
-              </div>
-            </div>
-            <div class="row justify-center">
-              <div class="col-11 q-mt-lg q-mx-md">
-                <back-web-site-button />
-              </div>
-            </div>
-          </div>
+          </q-intersection>
 
         </div>
 
@@ -106,7 +111,7 @@ export default defineComponent({
   },
   methods: {
     hideLoad() {
-      setTimeout(() => {this.loading = false}, 1000)
+      this.loading = false
     },
   },
 })
